af69dbef35584e3c07897eefaa5a3b817941730b
tom <tom@antivapor.net>
2014-06-15 06:08:45 -0700

    adds generated gdx setup

---
+++ android/src/com/starvapor/tarrare/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.starvapor.tarrare.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.starvapor.tarrare.tarrare2d;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new tarrare2d(), config);
+	}
+}
---
+++ desktop/src/com/starvapor/tarrare/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.starvapor.tarrare.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.starvapor.tarrare.tarrare2d;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new tarrare2d(), config);
+	}
+}
